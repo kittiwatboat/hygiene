f86f3b4e25beab60fcc2297e9ac7888f5af9942c
kittiwatboat <86706288+kittiwatboat@users.noreply.github.com>
2026-06-04 14:29:49 +0700

    แก้ไข

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,3 @@ use App\Http\Controllers\Backend\DashboardController;
 Route::get('/', function () {
     return view('welcome');
 });
-<<<<<<< HEAD
-Route::get('/login', function () {
-    return view('backend.page.login');
-})->name('login');
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-=======
->>>>>>> parent of 96fb664 (add login function)
