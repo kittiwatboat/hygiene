caaf21f868ec2efe6446705cc469556516dce5e4
kittiwatboat <86706288+kittiwatboat@users.noreply.github.com>
2026-06-04 14:33:22 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,5 @@ use App\Http\Controllers\Backend\DashboardController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
