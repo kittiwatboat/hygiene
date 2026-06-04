d0fb2005378c5cd316aac26bbcc39157c30227a0
kittiwatboat <86706288+kittiwatboat@users.noreply.github.com>
2026-06-04 14:23:34 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/login', function () {
+    return view('backend.page.login');
+})->name('login');
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
