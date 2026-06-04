1251a74d02b2dddeccd2db37ddd85907b182b510
kittiwatboat <86706288+kittiwatboat@users.noreply.github.com>
2026-06-04 15:03:34 +0700

    cdhw-

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -14,11 +14,11 @@ class DashboardController extends Controller
 
     public function index(Request $request)
     {
-        dd('aaa');
+        // dd('aaa');
         $navs = [
             '0' => ['url' => "javascript:void(0)", 'name' => "Dashboard", "last" => 0],
         ];
-        return view("$this->prefix.pages.$this->folder.index", [
+        return view("$this->prefix.page.$this->folder.index", [
             'prefix' => $this->prefix,
             'folder' => $this->folder,
             'segment' => $this->segment,
