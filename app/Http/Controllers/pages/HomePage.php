7265fd8d0878c709e9b09be5e68d0d9a3e5bcef4
kittiwatboat <86706288+kittiwatboat@users.noreply.github.com>
2026-06-08 10:33:24 +0700

    Update HomePage.php

--- app/Http/Controllers/pages/HomePage.php
+++ app/Http/Controllers/pages/HomePage.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class HomePage extends Controller
 {
   public function index()
-{
-    return view('content.pages.test');
-}
+  {
+    return view('content.pages.pages-home');
+  }
 }
