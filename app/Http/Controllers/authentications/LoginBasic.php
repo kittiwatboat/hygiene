f885b8d285a7253103c055bec3f48ebe677ce893
hygiene <kittiwatzero@gmail.com>
2026-06-09 09:12:03 +0700

    Update LoginBasic.php

--- app/Http/Controllers/authentications/LoginBasic.php
+++ app/Http/Controllers/authentications/LoginBasic.php
@@ -16,4 +16,15 @@ class LoginBasic extends Controller
     $pageConfigs = ['myLayout' => 'blank'];
     return view('content.authentications.auth-login-basic', ['pageConfigs' => $pageConfigs]);
   }
+
+  public function login(Request $request)
+  {
+    $credentials = $request->only('email', 'password');
+
+    if (Auth::attempt($credentials)) {
+      return redirect()->route('dashboard');
+    }
+
+    return redirect()->route('login')->with('error', 'อีเมลหรือรหัสผ่านไม่ถูกต้อง');
+  }
 }
