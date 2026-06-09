651cb8fed089637f62bd240393b38872796862d6
hygiene <kittiwatzero@gmail.com>
2026-06-09 11:37:34 +0700

    Update footer.blade.php

--- resources/views/layouts/sections/footer/footer.blade.php
+++ resources/views/layouts/sections/footer/footer.blade.php
@@ -1,4 +1,4 @@
-@php
+{{-- @php
 $containerFooter =
 isset($configData['contentLayout']) && $configData['contentLayout'] === 'compact'
 ? 'container-xxl'
@@ -21,6 +21,29 @@ isset($configData['contentLayout']) && $configData['contentLayout'] === 'compact
         <a href="{{ config('variables.moreThemes') ? config('variables.moreThemes') : '#' }}" target="_blank" class="footer-link me-4">More Themes</a>
         <a href="{{ config('variables.documentation') ? config('variables.documentation') . '/laravel-introduction.html' : '#' }}" target="_blank" class="footer-link me-4">Documentation</a>
         <a href="{{ config('variables.support') ? config('variables.support') : '#' }}" target="_blank" class="footer-link">Support</a>
+      </div>
+    </div>
+  </div>
+</footer>
+<!-- / Footer --> --}}
+
+@php
+$containerFooter =
+isset($configData['contentLayout']) && $configData['contentLayout'] === 'compact'
+? 'container-xxl'
+: 'container-fluid';
+@endphp
+
+<!-- Footer-->
+<footer class="content-footer footer bg-footer-theme">
+  <div class="{{ $containerFooter }}">
+    <div class="footer-container d-flex align-items-center justify-content-center py-4">
+      <div class="text-body text-center">
+        &#169;
+        <script>
+          document.write(new Date().getFullYear());
+        </script>
+        Created by KittiwatDev
       </div>
     </div>
   </div>
