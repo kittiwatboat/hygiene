1a723e3735fa10bda9ce0bb40b58eabffb0e2e7d
hygiene <kittiwatzero@gmail.com>
2026-06-09 11:09:15 +0700

    Update MachineController.php

--- app/Http/Controllers/machines/MachineController.php
+++ app/Http/Controllers/machines/MachineController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Machines;
 
+use App\Http\Controllers\Controller;
 use App\Models\Machine;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
