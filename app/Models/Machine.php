9cbe148473a37aa878d21da27b10927ad0109d94
hygiene <kittiwatzero@gmail.com>
2026-06-09 11:13:32 +0700

    Update Machine.php

--- app/Models/Machine.php
+++ app/Models/Machine.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class VendingMachine extends Model
+class Machine extends Model
 {
     use SoftDeletes;
 
