0ee60ed46b7c58d6ab19d9cfa18d1da649cd91aa
hygiene <kittiwatzero@gmail.com>
2026-06-18 11:28:21 +0700

    Update ProductController.php

--- app/Http/Controllers/Products/ProductController.php
+++ app/Http/Controllers/Products/ProductController.php
@@ -28,7 +28,7 @@ class ProductController extends Controller
     public function store(Request $request)
     {
       try {
-        return $request->all();
+        dd($request->all());
         $validated = $request->validate(
     [
         'code' => ['nullable', 'string', 'max:100', 'unique:products,code'],
