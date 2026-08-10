4ee53c7c176c62785dfe821c4f71ca3e35c05406
hygiene <kittiwatzero@gmail.com>
2026-08-10 19:13:31 +0700

    Update edit.blade.php

--- resources/views/content/pages/frontend/translations/edit.blade.php
+++ resources/views/content/pages/frontend/translations/edit.blade.php
@@ -437,7 +437,7 @@
 
                     <hr class="my-4">
 
-                    @include('content.pages.frontend.translations.partials.translation-table.php', [
+                    @include('content.pages.frontend.translations.partials.translation-table', [
                         'rows' => $selectRows,
                     ])
                 </div>
